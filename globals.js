55151691798f24466ba7caeb82e7a9bbbab22b62
miusuncle <miusuncle@gmail.com>
2016-10-13 12:36:48 +0800

    update: globals.js

--- globals.js
+++ globals.js
@@ -1,1 +1,13 @@
-global.debug = console.log.bind(console);
+global.debug = (() => {
+    const log = console.log.bind(console);
+
+    return function () {
+        log('========================================');
+        log.apply(null, Array.from(arguments).map(item => {
+            return (typeof item === 'object'
+                ? JSON.stringify(item, null, 2)
+                : item);
+        }));
+        log('========================================\n');
+    };
+})();
