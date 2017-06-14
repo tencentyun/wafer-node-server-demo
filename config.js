ad10b0df94bd59fd936ebf8ced0408a038a6e6a7
Qinsi ZHU <qszhu@sightp.com>
2017-06-15 00:35:37 +0800

    add cos auth to routes

--- config.js
+++ config.js
@@ -5,4 +5,12 @@ module.exports = {
      * Node 服务器启动端口，如果是自行搭建，请保证负载均衡上的代理地址指向这个端口
      */
     port: '5757',
+    /**
+     * 对象存储服务(COS)相关配置
+     */
+    cos_auth: {
+        appId: '',
+        secretId: '',
+        secretKey: ''
+    }
 };
